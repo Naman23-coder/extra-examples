95bb7bf6028ce18f26da41993e53716317909b16
Naman sharma <78961713+Naman23-coder@users.noreply.github.com>
2021-04-24 22:04:00 +0530

    Create speak.py

---
+++ speak.py
@@ -0,0 +1,5 @@
+# author :- Naman Sharma (namansharma232009@gmail.com)
+# this is speak example for VirtualAssistant
+import VirtualAssistant as va # import VirtualAssiant Package,find more info at https://www.github.com/Naman23-coder/VirtualAssistant
+va.speak('Hello World') # speaking in default voice
+va.speak('Hello World',voice=1) # using different voice,you can use findvoices(voice = <'voicenumber'>) to find voices
